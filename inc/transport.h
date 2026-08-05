33ecf3dd27609413b159908ff75be7c7bbe784bd
XTheocharis <162344168+XTheocharis@users.noreply.github.com>
2026-08-04 23:54:35 -0700

    fix(transport): delete dead whad_transport_transfer declaration

--- inc/transport.h
+++ inc/transport.h
@@ -61,11 +61,8 @@ typedef struct {
 
 void whad_transport_init(whad_transport_cfg_t *p_transport_cfg);
 whad_result_t whad_transport_data_received(uint8_t *p_data, int size);
-whad_result_t whad_transport_transfer(void);
 whad_result_t whad_transport_send_pending(void);
 void whad_transport_data_sent(void);
-whad_result_t whad_transport_send_byte(uint8_t data);
-int whad_transport_send(uint8_t *p_data, int size);
 
 whad_result_t whad_transport_get_message(uint8_t *p_buffer, int *p_size);
 whad_result_t whad_transport_send_message(uint8_t *p_message, int size);
